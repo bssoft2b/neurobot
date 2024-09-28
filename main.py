16e047238eb6742d5f7ecc9f7f3ebb78da32cb42
Kan <bssoft2b@gmail.com>
2024-09-28 15:14:43 +0300

    qwdewrgtr

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-import misc
-import handlers
 import asyncio
 import logging
 
+from aiogram import Bot,Dispatcher
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.client.bot import DefaultBotProperties
+from aiogram.utils.chat_action import ChatActionMiddleware
+
+import config
+from handlers import router
+
 async def main():
-    await misc.bot.delete_webhook(drop_pending_updates=True)
-    await misc.dp.start_polling(misc.bot, allowed_updates=misc.dp.resolve_used_update_types())
-
-
-if __name__ == "__main__":
+    
+    bot=Bot(token=config.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp=Dispatcher(storage=MemoryStorage())
+    dp.include_router(router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    dp.message.middleware(ChatActionMiddleware())
+    await dp.start_polling(bot,allowed_updates=dp.resolve_used_update_types())
+    
+if __name__=="__main__":
     logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
+    
---
+++ states.py
@@ -0,0 +1,7 @@
+from aiogram.fsm.state import StatesGroup,State
+
+class Gen(StatesGroup):
+    text_prompt=State()
+    img_prompt=State()
+    
+    
